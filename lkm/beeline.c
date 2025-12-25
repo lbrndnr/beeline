bd61ac456094880440fd375fc4df4f73ccc6da71
Laurin Brandner <mail@laurinbrandner.ch>
2025-12-25 14:35:10 +0100

    lkm: cleanup

--- lkm/beeline.c
+++ lkm/beeline.c
@@ -2,83 +2,16 @@
 
 #include <linux/bpf.h>
 #include <linux/btf.h>
-#include <linux/btf_ids.h>
-#include <linux/errno.h>
-#include <linux/filter.h>
-#include <linux/module.h>
-#include <linux/types.h>
 
-static const char base64url_table[65] =
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-
-__bpf_kfunc_start_defs();
-
-__bpf_kfunc int bl_base64url_encode(const u8 *src,
-								  u32 src__sz,
-								  char *dst,
-								  u32 dst__sz)
-{
-	if (dst__sz < 4*(src__sz/3))
-		return -EINVAL;
-
-	u32 ac = 0;
-	int bits = 0;
-	int i;
-	char *cp = dst;
-
-	for (i = 0; i < src__sz; i++) {
-		ac = (ac << 8) | src[i];
-		bits += 8;
-		do {
-			bits -= 6;
-			*cp++ = base64url_table[(ac >> bits) & 0x3f];
-		} while (bits >= 6);
-	}
-	if (bits) {
-		*cp++ = base64url_table[(ac << (6 - bits)) & 0x3f];
-		bits -= 6;
-	}
-
-	return cp - dst;
-}
-
-__bpf_kfunc int bl_base64url_decode(const u8 *src,
-								  u32 src__sz,
-								  char *dst,
-								  u32 dst__sz)
-{
-    if (dst__sz < 3*(src__sz/4))
-		return -EINVAL;
-
-    u32 ac = 0;
-	int bits = 0;
-	int i;
-	char *bp = dst;
-
-	for (i = 0; i < src__sz; i++) {
-		const char *p = strchr(base64url_table, src[i]);
-
-		if (p == NULL || src[i] == 0)
-			return -1;
-		ac = (ac << 6) | (p - base64url_table);
-		bits += 6;
-		if (bits >= 8) {
-			bits -= 8;
-			*bp++ = (u8)(ac >> bits);
-		}
-	}
-	if (ac & ((1 << bits) - 1))
-		return -1;
-	return bp - dst;
-}
-
-__bpf_kfunc_end_defs();
+#include "base64url.h"
 
 BTF_KFUNCS_START(beeline_kfunc_btf_ids)
+
 BTF_ID_FLAGS(func, bl_base64url_encode, KF_RCU)
 BTF_ID_FLAGS(func, bl_base64url_decode, KF_RCU)
+
 BTF_KFUNCS_END(beeline_kfunc_btf_ids)
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("A module that adds coding functionality.");
+MODULE_DESCRIPTION("The beeline kernel module exposes coding schemes to eBPF.");
 MODULE_VERSION("1.0");
---
+++ lkm/hpack.h
@@ -0,0 +1,7 @@
+__bpf_kfunc_start_defs();
+
+__bpf_kfunc int bl_hpack_decode(const u8 *src, u32 src__sz, char *dst, u32 dst__sz) {
+    return 0;
+}
+
+__bpf_kfunc_end_defs();
