46d8167e5abf668c38f4c6f8cc2e88e4bf5ba986
Joel Fischer <fischjoe@student.ethz.ch>
2026-02-16 08:18:05 +0100

    Add post requests, arbitrary routes and return handle for server task.

--- utils/src/server.rs
+++ utils/src/server.rs
@@ -5,8 +5,10 @@ use axum::{
     http::{HeaderMap, StatusCode},
     response::IntoResponse,
     routing::get,
+    routing::post,
 };
 use tracing::debug;
+use tokio::task::JoinHandle;
 
 async fn echo(headers: HeaderMap, body: Bytes) -> Result<impl IntoResponse, StatusCode> {
     if let Ok(body) = String::from_utf8(body.to_vec()) {
@@ -32,4 +34,43 @@ pub async fn launch<A: tokio::net::ToSocketAddrs>(addr: A) -> Result<()> {
     });
 
     Ok(())
+}
+
+pub async fn launch_cancellable<A: tokio::net::ToSocketAddrs>(addr: A) -> Result<JoinHandle<()>> {
+    let app = Router::new()
+        .route(
+            "/{*path}",
+            get(move |_: HeaderMap, body: Bytes| {
+                let res_hdrs = HeaderMap::new();
+                echo(res_hdrs, body)
+            }),
+        )
+        .route(
+            "/",
+            get(move |_: HeaderMap, body: Bytes| {
+                let res_hdrs = HeaderMap::new();
+                echo(res_hdrs, body)
+            }),
+        )
+        .route(
+            "/{*path}",
+            post(move |_: HeaderMap, body: Bytes| {
+                let res_hdrs = HeaderMap::new();
+                echo(res_hdrs, body)
+            }),
+        )
+        .route(
+            "/",
+            post(move |_: HeaderMap, body: Bytes| {
+                let res_hdrs = HeaderMap::new();
+                echo(res_hdrs, body)
+            }),
+        );
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    let handle = tokio::spawn(async move {
+        axum::serve(listener, app).await.unwrap();
+    });
+
+    Ok(handle)
 }
